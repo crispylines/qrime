a34890d162bdb1310e1e0391e3d8a1e2be3b769f
Drew <47359612+CrispLines@users.noreply.github.com>
2022-10-30 20:03:55 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-       
+        
         <p>
         <a href="https://twitter.com/crispylines">qrime.eth is loading..</a>
         </p>
