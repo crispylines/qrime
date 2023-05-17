998f2944c35e1fc344a255d1c5c98896ad805f03
Andrew <andrew.ortiz@deluxe.com>
2023-05-17 12:50:53 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,5 @@
 
 import './App.css';
-import React from react 
-
-const SocialMediaLinks = () => {
-  const handleTwitterClick = () => {
-    // Handle Twitter click event
-    // Replace the URL with your desired Twitter link
-    window.open('https://twitter.com');
-  };
-
-  const handleInstagramClick = () => {
-    // Handle Instagram click event
-    // Replace the URL with your desired Instagram link
-    window.open('https://instagram.com');
-  };
-
-
-}
 
 function App() {
   return (
@@ -26,19 +9,6 @@ function App() {
         <p>
         qrime.eth is building something... 
         </p>
-
-        <div>
-      <img
-        src="path/to/twitter-logo.png"
-        alt="Twitter Logo"
-        onClick={handleTwitterClick}
-      />
-      <img
-        src="path/to/instagram-logo.png"
-        alt="Instagram Logo"
-        onClick={handleInstagramClick}
-      />
-    </div>
        
       </header>
     </div>
