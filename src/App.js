58f403c6782e5fb1a192c6e69954752dbe275214
Drew <47359612+CrispLines@users.noreply.github.com>
2023-09-25 14:08:22 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
-
+import React from 'react';
 import './App.css';
+import { Link } from 'react-router-dom'; // Import Link for navigation
 
 function App() {
   return (
     <div className="App font-link">
       <header className="App-header">
-        
         <p>
-        qrime is coming...
+          qrime is coming...
         </p>
-       
+        {/* Add a navigation link */}
+        <nav>
+          <ul>
+            <li>
+              <Link to="/another-page">Go to Another Page</Link>
+            </li>
+          </ul>
+        </nav>
       </header>
     </div>
   );
