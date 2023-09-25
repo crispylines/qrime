9fa47e61ce33ca49254491cd3e3f415328a77b67
Drew <47359612+CrispLines@users.noreply.github.com>
2023-09-25 13:19:51 -0500

    test button

--- src/App.js
+++ src/App.js
@@ -1,15 +1,31 @@
 
 import './App.css';
+import { useHistory } from 'react-router-dom';
+import RedirectButton from './RedirectButton';
+
+function RedirectButton() {
+  const history = useHistory();
+
+  const handleClick = () => {
+    // Redirect to a different page (e.g., '/new-page')
+    history.push('/new-page');
+  };
+
+  return (
+    <button onClick={handleClick}>
+      Go to New Page
+    </button>
+  );
+}
 
 function App() {
   return (
     <div className="App font-link">
       <header className="App-header">
-        
         <p>
-        qrime is coming...
+          qrime is coming...
         </p>
-       
+        <RedirectButton />
       </header>
     </div>
   );
