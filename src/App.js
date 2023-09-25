aff94a893f059b734022ae380ca96b55ac56d18a
Drew <47359612+CrispLines@users.noreply.github.com>
2023-09-25 13:57:07 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,32 +1,17 @@
-import React from 'react';
+
 import './App.css';
-import { BrowserRouter as Router, Link, Route } from 'react-router-dom';
-import Shirts from './Shirts'; // Import the Shirts component
 
 function App() {
   return (
-    <Router>
-      <div className="App font-link">
-        <header className="App-header">
-          <p>qrime is coming...</p>
-          <nav>
-            <ul>
-              <li>
-                <Link to="/">Home</Link>
-              </li>
-              <li>
-                <Link to="/shirts">Shirts</Link>
-              </li>
-              {/* Add more navigation links as needed */}
-            </ul>
-          </nav>
-        </header>
-        <Switch>
-          <Route exact path="/shirts" component={Shirts} />
-          {/* Define other routes here */}
-        </Switch>
-      </div>
-    </Router>
+    <div className="App font-link">
+      <header className="App-header">
+        
+        <p>
+        qrime is coming...
+        </p>
+       
+      </header>
+    </div>
   );
 }
 
