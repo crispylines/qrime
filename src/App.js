15dd9421dc62817a596f4fa0a88f2a669ae6719b
Drew <47359612+CrispLines@users.noreply.github.com>
2024-03-05 19:21:30 -0600

    Onclick event
    twitter click

--- src/App.js
+++ src/App.js
@@ -2,10 +2,14 @@ import React from 'react';
 import './App.css';
 
 function App() {
+  const handleClick = () => {
+    window.open('https://twitter.com/crispylines', '_blank');
+  };
+
   return (
     <div className="App font-link">
       <header className="App-header">
-        <p className="zoom-on-hover">
+        <p className="zoom-on-hover" onClick={handleClick}>
           qrime is coming....
         </p>
       </header>
