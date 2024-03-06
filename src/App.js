f438e15320337481023f53eae591249af9bea24c
Drew <47359612+CrispLines@users.noreply.github.com>
2024-03-05 19:36:52 -0600

    starts test
    moving stars

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ function App() {
         <p className="zoom-on-hover" onClick={handleClick}>
           qrime is coming....
         </p>
+        <div className="stars"></div>
       </header>
     </div>
   );
