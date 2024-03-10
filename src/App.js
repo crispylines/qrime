b0dd0f5d8a4a53cecadd914be3fd4d06d930202b
Drew <47359612+CrispLines@users.noreply.github.com>
2024-03-09 20:30:26 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   const [style, setStyle] = useState({});
 
   const handleMouseEnter = () => {
-    const newPositionX = Math.random() * (-300) * 100;
+    const newPositionX = Math.random() * (-400);
     const newPositionY = Math.random() * 100;
     setStyle({
       transform: `translate(${newPositionX}%, ${newPositionY}%) scale(1.4)`,
