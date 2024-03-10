11ec8f0c1c67f1c47a74ce161cc8d4ecbf45bec9
Drew <47359612+CrispLines@users.noreply.github.com>
2024-03-09 21:09:01 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,15 +6,15 @@ function App() {
 
   const handleMouseEnter = () => {
     const positions = [
-      { x: Math.random() * 25, y: Math.random() * 25 },
-      { x: Math.random() * 25 + 25, y: Math.random() * 25 },
-      { x: Math.random() * 25, y: Math.random() * 25 + 25 },
-      { x: Math.random() * 25 + 25, y: Math.random() * 25 + 25 },
+      { x: Math.random() * 10, y: Math.random() * 15 },
+      { x: Math.random() * 5, y: Math.random() * 20 },
+      { x: Math.random() * 20, y: Math.random() * 10 },
+      { x: Math.random() * 15, y: Math.random() * 5 },
     ];
 
     const newPosition = positions[Math.floor(Math.random() * positions.length)];
     setStyle({
-      transform: `translate(${newPosition.x}%, ${newPosition.y}%) scale(1.2)`,
+      transform: `translate(${newPosition.x}%, ${newPosition.y}%) scale(1.5)`,
     });
   };
 
