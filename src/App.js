4df3a2f1827a3ee546e405a27370a1d646e706ad
Drew <47359612+CrispLines@users.noreply.github.com>
2024-03-14 14:48:17 -0500

    fix?

--- src/App.js
+++ src/App.js
@@ -3,13 +3,27 @@ import './App.css';
 
 function App() {
   const [isButtonOnFire, setIsButtonOnFire] = useState(false);
+  const [style, setStyle] = useState({});
 
   const handleMouseEnter = () => {
     setIsButtonOnFire(true);
+
+    const positions = [
+      { x: Math.random() * -100, y: Math.random() * 150 },
+      { x: Math.random() * 50, y: Math.random() * -200 },
+      { x: Math.random() * -200, y: Math.random() * 100 },
+      { x: Math.random() * 150, y: Math.random() * -50 },
+    ];
+
+    const newPosition = positions[Math.floor(Math.random() * positions.length)];
+    setStyle({
+      transform: `translate(${newPosition.x}%, ${newPosition.y}%) scale(1.6)`,
+    });
   };
 
   const handleMouseLeave = () => {
     setIsButtonOnFire(false);
+    setStyle({});
   };
 
   const handleLinkClick = (link) => {
@@ -37,6 +51,7 @@ function App() {
         </nav>
         <p
           className="text"
+          style={style}
           onClick={() => handleLinkClick('https://twitter.com/notqrime')}
         >
           qrime is coming
